5486ce0c20873ab41b95721bf1e344c21940bda0
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-12-21 17:26:19 -0300

    fix: enhance monthly tracking dataframe initialization logic

--- src/prbr25_db_ops/reporting/lock/initialization.py
+++ src/prbr25_db_ops/reporting/lock/initialization.py
@@ -14,9 +14,14 @@ def create_monthly_tracking_dataframe(start_year: int, start_month: int) -> Data
     year = start_year
     month = start_month
 
-    for _ in range(12):
+    for i in range(12):
         data.append(
-            {"year": year, "month": month, "report": False, "updated_values": False}
+            {
+                "year": year,
+                "month": month,
+                "report": i == 0,
+                "updated_values": i == 0,
+            }
         )
 
         month += 1
