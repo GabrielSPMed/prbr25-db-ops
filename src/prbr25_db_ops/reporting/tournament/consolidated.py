1430d2dd5749ff1da7466fa2c1bd4b125ba1d6e4
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-12-23 20:23:41 -0300

    Bump version to 0.8.0 and add prbr25-weights dependency
    - Updated version in pyproject.toml from 0.7.1 to 0.8.0.
    - Added prbr25-weights as a dependency.
    - Modified the SQL query in consolidated.py to select tournament_name from raw_events and added a call to add_grade_column to enhance the DataFrame.

--- src/prbr25_db_ops/reporting/tournament/consolidated.py
+++ src/prbr25_db_ops/reporting/tournament/consolidated.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from pandas import DataFrame
 from prbr25_rds_client.postgres import Postgres
+from prbr25_weights.weights import add_grade_column
 
 
 def get_validated_tournaments(
@@ -10,12 +11,13 @@ def get_validated_tournaments(
 ) -> DataFrame:
     start_date = datetime(year, month, 1)
     end_date = start_date + relativedelta(months=1)
-    query = f"""SELECT c.tournament_name, c.event_name, c.address_state, c.start_at, c.num_entrants, r.value, r.n_dqs, c.url
+    query = f"""SELECT r.tournament_name, r.event_name, r.address_state, r.start_at, r.num_entrants, c.value AS score, c.n_dqs, r.url
                 FROM raw_events AS r
                 INNER JOIN consolidated_events AS c ON r.id = c.id
                 WHERE r.start_date BETWEEN '{start_date}' AND '{end_date}'
             """
     df = sql.query_db(query, "raw_events")
+    df = add_grade_column(df)
     if save:
         df.to_csv(f"{path}/consolidated_events.csv", index=False)
     return df
