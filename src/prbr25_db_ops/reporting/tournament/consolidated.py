124917593fd7db0047ff1076ec74f3f6afcd0269
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-12-23 23:08:43 -0300

    feat: update version to 0.9.3 and modify get_validated_tournaments function to return a tuple of DataFrame and Series

--- src/prbr25_db_ops/reporting/tournament/consolidated.py
+++ src/prbr25_db_ops/reporting/tournament/consolidated.py
@@ -1,23 +1,25 @@
 from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from prbr25_rds_client.postgres import Postgres
 from prbr25_weights.weights import add_grade_column
 
 
 def get_validated_tournaments(
     month: int, year: int, sql: Postgres, save: bool = False, path: str = ""
-) -> DataFrame:
+) -> tuple[DataFrame, Series]:
     start_date = datetime(year, month, 1)
     end_date = start_date + relativedelta(months=1)
-    query = f"""SELECT r.tournament_name, r.event_name, r.address_state, r.start_at, r.num_entrants, c.value AS score, c.n_dqs, r.url
+    query = f"""SELECT r.tournament_name, r.event_name, r.address_state, r.start_at, r.num_entrants, c.value AS score, c.n_dqs, r.url, r.id
                 FROM raw_events AS r
                 INNER JOIN consolidated_events AS c ON r.id = c.id
                 WHERE r.validated AND r.start_at BETWEEN '{start_date}' AND '{end_date}'
             """
     df = sql.query_db(query, "raw_events")
     df = add_grade_column(df)
+    id_series = df["id"]
+    df.drop(["id"], axis=1)
     if save:
         df.to_csv(f"{path}/consolidated_events.csv", index=False)
-    return df
+    return df, id_series
